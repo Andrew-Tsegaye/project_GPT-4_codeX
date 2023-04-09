d91023db0f3bceaa35df7c94a19cfa06041103ba
Andrew Tsegaye <andrewtsegaye7@gmail.com>
2023-04-09 03:18:28 -0700

    new changes

--- client/app.js
+++ client/app.js
@@ -81,7 +81,7 @@ const handleSubmit = async e => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://openai-gpt-4-codex.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
